82e110d3bf009801f4cdcc07465e57fdabfdc011
Niki Roo <niki@nikiroo.be>
2017-08-22 22:14:43 +0200

    Update to the gopher mini-test service

--- gopher.sh
+++ gopher.sh
@@ -2,6 +2,10 @@
 
 # Note: bash is required, sh will fail to output anything to the client
 # Probably related to interactions beteween & and output redirections
+# 
+# It seems the timeout on netcat does not work when listening...
+# So you will have to either kill the process or make a last connection
+# (which will not be processed)
 
 # $0 [prog] ([port])
 # You can use the evironment variable $QUERY_STRING for the query
@@ -14,15 +18,17 @@
 
 prog="$1"
 port="$2"
+defn="$3"
 [ "$port"    = "" ] && port=70
 
 if [ "$prog" = "" ]; then
 	rm "$PIDF"
-	echo "Syntax: $0 [prog] ([port])" >&2
+	echo "Syntax: $0 [prog] ([port]) (default non-7)" >&2
 	echo '	You can use the evironment variable $QUERY_STRING for the query' >&2
 	echo '	The default port is 70' >&2
+	echo '	A default message for non-service (7) request can be given' >&2
 	echo '	A filename will be displayed, you can delete it to stop the service' >&2
-	echo '	(with a grace time of $TIMEOUT seconds)' >&2
+	echo '	(you still have to make a last connection on $port)' >&2
 	exit 1
 fi
 
@@ -33,7 +39,17 @@ if [ ! -d "$tmpd" ]; then
 	exit 2
 fi
 
-touch "$PIDF"
+echo "#/bin/sh
+# PID: $$
+# PID File (this file): $PIDF
+# You can stop the service by deleting this file then making a last
+# connection (which will not be processed) to the service:
+rm \"$PIDF\"
+nc -q0 $ADDR $port 2>/dev/null </dev/null
+" > "$PIDF"
+
+chmod u+rx "$PIDF"
+
 echo "$PIDF"
 
 i=0
@@ -44,9 +60,20 @@ while [ -e "$PIDF" ]; do
 	rm -f "$fifo" 2>/dev/null
 	mkfifo "$fifo"
 	< "$fifo" nc -l -q0 -w"$TIMEOUT" "$ADDR" -p "$port" | (
-		read -r query
-		query="`echo "$query" | cut -f2 -d'	' | sed 's:[\r\n]::g'`"
-		QUERY_STRING="$query" "$prog" 2>> "$LOG" &
+		if [ -e "$PIDF" ]; then
+			read -r query
+			selector="`echo "$query" | cut -f1 -d'	' | sed 's:[\r\n]::g'`"
+			query="`echo "$query" | cut -f2 -d'	' | sed 's:[\r\n]::g'`"
+			if [ "$query" = "" ]; then
+				if [ "$defn" = "" ]; then
+					echo "7Search	$selector	$ADDR	$port"
+				else
+					echo "$defn"
+				fi
+			else
+				QUERY_STRING="$query" "$prog" 2>> "$LOG" &
+			fi
+		fi
 	) > "$fifo"
 done
 rm -rf "$tmpd" 2>/dev/null
