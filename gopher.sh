4a19c3b881f510ea4335b6a5552963117b30234c
Niki Roo <niki@nikiroo.be>
2017-08-22 21:54:47 +0200

    Update gopher.sh mini test service

--- gopher.sh
+++ gopher.sh
@@ -1,19 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
-# $0 [prog ($1 will be the query)] ([port])
+# Note: bash is required, sh will fail to output anything to the client
+# Probably related to interactions beteween & and output redirections
+
+# $0 [prog] ([port])
+# You can use the evironment variable $QUERY_STRING for the query
 
 [ "$ADDR"    = "" ] && ADDR=127.0.0.1
-[ "$LOG"     = "" ] && LOG=/tmp/log.err
+[ "$LOG"     = "" ] && LOG=/tmp/gopher.service.err
 [ "$PIDF"    = "" ] && PIDF="`mktemp`"
 [ "$TIMEOUT" = "" ] && TIMEOUT=10
 [ "$MAXCON"  = "" ] && MAXCON=1024
+
 prog="$1"
 port="$2"
 [ "$port"    = "" ] && port=70
 
 if [ "$prog" = "" ]; then
 	rm "$PIDF"
-	echo "Syntax: $0 [prog (\$1 will be the query)] ([port])" >&2
+	echo "Syntax: $0 [prog] ([port])" >&2
+	echo '	You can use the evironment variable $QUERY_STRING for the query' >&2
+	echo '	The default port is 70' >&2
+	echo '	A filename will be displayed, you can delete it to stop the service' >&2
+	echo '	(with a grace time of $TIMEOUT seconds)' >&2
 	exit 1
 fi
 
@@ -32,16 +41,12 @@ while [ -e "$PIDF" ]; do
 	i=`expr $i + 1`
 	[ $i -gt "$MAXCON" ] && i=1
 	fifo="$tmpd/fifo.$i"
-	rm -f "$fifo"
+	rm -f "$fifo" 2>/dev/null
 	mkfifo "$fifo"
 	< "$fifo" nc -l -q0 -w"$TIMEOUT" "$ADDR" -p "$port" | (
 		read -r query
-		query="`echo "$query" | cut -f2 -d'	'`"
-		(
-			"$prog" "$query" 2>> "$LOG"
-			rm "$fifo"
-		)&
+		query="`echo "$query" | cut -f2 -d'	' | sed 's:[\r\n]::g'`"
+		QUERY_STRING="$query" "$prog" 2>> "$LOG" &
 	) > "$fifo"
 done
-rm -rf "$tmpd"
-
+rm -rf "$tmpd" 2>/dev/null
