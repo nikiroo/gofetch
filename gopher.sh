1279ec4539171e2eedfe253be867c23b7d5e56b8
Niki Roo <niki@nikiroo.be>
2020-08-03 22:52:29 +0200

    gopher.sh: hack gemini support into it

--- gopher.sh
+++ gopher.sh
@@ -21,9 +21,11 @@
 # ENV variables:
 #	LINK_COLOR: escape sequences colour (def: 2)
 #		- : means no escape sequence
+# 		* : means bold
 #		1 : means colour 1
 #		2 : means colour 2
 #		[...]
+#	TITLE_COLOR: escape sequences colour (def: *) for titles (same as above)
 # 	INVERT    : invert the output for image viewing (for white backgrounds)
 # 		0 : do not invert (default)
 #		1 : invert 
@@ -38,6 +40,8 @@
 URI="$1"
 
 PREFIX="[0-9hIg+]"
+PREFIX_G="\(=> \)"
+PREFIX_GT="\(\#..\)"
 
 PROTOCOL="`echo $URI | sed 's|^\([^:]*://\)\?\([^:/]*\)\(:[^/]*\)\?/\?\(.*\)\?|\1|g'`"
 SERVER="`echo $URI | sed 's|^\([^:]*://\)\?\([^:/]*\)\(:[^/]*\)\?/\?\(.*\)\?|\2|g'`"
@@ -48,17 +52,21 @@ MODE=
 PORT="`echo "$PORT" | sed 's/^://'`"
 
 # Defaults:
+[ "$PROTOCOL" = "" ] && PROTOCOL=gopher://
 if [ "$PORT" = "" ];then
 	case "$PROTOCOL" in
-	http://)  PORT=80 ;;
-	https://) PORT=443;;
-	*)     PORT=70 ;;
+	http://)   PORT=80 ;;
+	https://)  PORT=443;;
+	gemini://) PORT=1965;;
+	gopher://) PORT=70 ;;
 	esac
 fi
 
 if [ "$MODE" = "" ]; then
 	# "" or dir-like selector? -> 1 ; if not -> 0 
 	echo "$SELECTOR" | grep "/$" >/dev/null && MODE=1 || MODE=0
+	echo "$SELECTOR" | grep "^0/" >/dev/null && MODE=0
+	echo "$SELECTOR" | grep "^1/" >/dev/null && MODE=1
 	[ "$SELECTOR" = "" ] && MODE=1
 	
 	# check explicit modes:
@@ -75,13 +83,32 @@ fi
 
 # can be "-" for no escape sequences
 [ "$LINK_COLOR" = ""  ] && LINK_COLOR=2
+[ "$TITLE_COLOR" = ""  ] && TITLE_COLOR='*'
 
 # Start and end link tags
 SL=
 EL=
 if [ "$LINK_COLOR" != "-" ]; then
-	SL="`tput setf $LINK_COLOR``tput setaf $LINK_COLOR`"
-	EL="`tput init`"
+	if [ "$LINK_COLOR" = '*' ]; then
+		ST="`tput smso`"
+		ET="`tput rmso`"
+	else
+		SL="`tput setf $LINK_COLOR``tput setaf $LINK_COLOR`"
+		EL="`tput sgr0``tput init`"
+	fi
+	export LESS="${LESS} -R"
+fi
+# Start and end title tags
+ST=
+ET=
+if [ "$TITLE_COLOR" != "-" ]; then
+	if [ "$TITLE_COLOR" = '*' ]; then
+		ST="`tput smso`"
+		ET="`tput rmso`"
+	else
+		ST="`tput setf $TITLE_COLOR``tput setaf $TITLE_COLOR`"
+		ET="`tput sgr0``tput init`"
+	fi
 	export LESS="${LESS} -R"
 fi
 
@@ -96,25 +123,58 @@ fi
 # Display a gopher menu for the given resource
 cat_menu() {
 	i=0
-	cat "$1" | grep "^i\|^$PREFIX" | sed 's:\\:\\\\\\\\:g' | while read ln; do
+	cat "$1" | sed 's:\\:\\\\\\\\:g' | while read ln; do
 		if echo "$ln" | grep "^i" >/dev/null 2>&1; then
 			echo "$ln" | sed "s:^.\([^\t]*\).*$:	\1:g"
-		elif echo "$ln" | grep "^$PREFIX" >/dev/null 2>&1; then
+		elif echo "$ln" | grep "^\($PREFIX\)\|\($PREFIX_G\)" >/dev/null 2>&1; then
 			i=`expr $i + 1`
 			[ $i -le 9 ] && i=0$i 
 			field="`echo "$ln" | cut -c1`"
-			case "$field" in
-				0) typ='TXT';;
-				1) typ='DIR';; # menu, actually
-				7) typ='(?)';; # query
-				8) typ='TEL';; # TELnet (not TELephone)
-				h) typ='WEB';; # HTML
-				I) typ='IMG';;
-				g) typ='GIF';;
-				+) typ='SVR';; # redundant server
-				*) typ='!!!';;
+			field3="`echo "$ln" | cut -c1-3`"
+			[ "$PROTOCOL" != gemini:// ] && field3=
+			
+			newln="`echo "$ln" | cut -c4-`"
+			case "$field3" in
+			'=> ')
+				typ='LNK' # gemini link
+				newln="`echo "$newln" | sed 's:\t: :g'`"
+				f1="`echo "$newln" | cut -f1  -d' '`" # link
+				f2="`echo "$newln" | cut -f2- -d ' '`" # detail
+				[ "$f1" = "$f2" ] && f2= || f2=" ($f2)"
+				
+				echo "${SL}${typ}${EL} $i	${SL}${f1}${EL}${f2}"
+			;;
+			*)
+				if [ "$PROTOCOL" = gopher:// ]; then
+					newln="`echo "$ln" | cut -c2-`"
+					newln="`echo "$newln" | sed "s:\\([^\t]*\\).*:\1:g"`"
+					case "$field" in
+						0) typ='TXT';;
+						1) typ='DIR';; # menu, actually
+						7) typ='(?)';; # query
+						8) typ='TEL';; # TELnet (not TELephone)
+						h) typ='WEB';; # HTML
+						I) typ='IMG';;
+						g) typ='GIF';;
+						+) typ='SVR';; # redundant server
+						*) typ='!!!';;
+					esac
+					echo "${SL}${typ}${EL} $i	${SL}${newln}${EL}"
+				else
+					echo "$ln"
+				fi
+			;;
 			esac
-			echo "$ln" | sed "s:^.\\([^\t]*\\).*:$typ $i	$SL\\1$EL:g"
+		elif echo "$ln" | grep "^$PREFIX_GT" >/dev/null 2>&1; then
+			field3="`echo "$ln" | cut -c1-3`"
+			case "$field3" in
+				'#'??)
+					echo "${ST}${ln}${ET}";;
+				*)
+					echo "$ln";;
+			esac
+		else
+			echo "$ln"
 		fi
 	done
 }
@@ -128,7 +188,7 @@ cat_menu() {
 # 	3 = server
 # 	4 = port
 getsel() {
-	cat "$1" | grep "^$PREFIX" | tail -n+"$2" | head -n 1 | cut -f"$3"
+	cat "$1" | grep "^$PREFIX\|\($PREFIX_G\)" | tail -n+"$2" | head -n 1 | cut -f"$3"
 }
 
 # Save page content to 'tmp' file
@@ -139,8 +199,19 @@ finish() {
 trap finish EXIT
 
 ok=true
-if [ "$PROTOCOL" = gopher:// -o "$PROTOCOL" = "" ]; then
+if [ "$PROTOCOL" = gopher:// ]; then
 	echo "$SELECTOR" | nc "$SERVER" "$PORT" > "$tmp" || ok=false
+elif [ "$PROTOCOL" = gemini:// ]; then
+	echo "${PROTOCOL}$SERVER:$PORT/$SELECTOR" \
+		| openssl s_client -crlf -quiet -connect $SERVER:$PORT > "$tmp" 2>/dev/null
+	sline="`cat "$tmp" | head -n1`"
+	<"$tmp" tail -n +2  > "$tmp".2
+	mv -f "$tmp".2 "$tmp"
+	if [ "`echo "$sline" | cut -c1,4- | sed 's:/.*::g'`" = 2text ]; then
+		MODE=1
+	else
+		MODE=0
+	fi
 else
 	if wget -h >/dev/null 2>&1; then
 		wget "${PROTOCOL}$SERVER:$PORT/$SELECTOR" -O "$tmp" >/dev/null 2>&1
@@ -186,13 +257,33 @@ download)
 		[ "$CHOICE" = q ] && exit 255 # force-quit
 		index="`expr 1 \* "$CHOICE" 2>/dev/null`"
 		if [ "$index" != "" ]; then
-			goto_server="`getsel "$tmp" $index 3`"
-			goto_sel="`getsel "$tmp" $index 2`"
-			goto_port="`getsel "$tmp" $index 4`"
-			goto_mode="`getsel "$tmp" $index 1 | cut -c1`"
-			echo "Digging to [$goto_server:$goto_port] $index [$goto_sel]..."
-			sh "$0" "$goto_server:$goto_port/$goto_mode/$goto_sel"
-			[ $? = 255 ] && exit 255 # force-quit
+			if [ "$PROTOCOL" = gemini:// ]; then
+				goto_gem="`getsel "$tmp" $index 1- | cut -c3-`"
+				goto_gem="`echo "$goto_gem" | sed 's:^\s*::g;s:\s*$::g'`"
+				goto_gem="`echo "$goto_gem" | cut -f1`"
+				
+				if ! echo "$goto_gem" | grep '.://' >/dev/null; then
+					if ! echo "$goto_gem" | grep '^/' >/dev/null; then
+						goto_gem="$SERVER:$PORT/$goto_gem"
+					else
+						goto_gem="$SERVER:$PORT/$SELECTOR/$goto_gem"
+					fi
+					goto_gem="gemini://$goto_gem"
+				fi
+				
+				echo "Digging to [$goto_gem]..."
+				
+				sh "$0" "$goto_gem"
+				[ $? = 255 ] && exit 255 # force-quit
+			else
+				goto_server="`getsel "$tmp" $index 3`"
+				goto_sel="`getsel "$tmp" $index 2`"
+				goto_port="`getsel "$tmp" $index 4`"
+				goto_mode="`getsel "$tmp" $index 1 | cut -c1`"
+				echo "Digging to $index [$goto_server:$goto_port] $goto_mode [$goto_sel]..."
+				sh "$0" "$goto_server:$goto_port/$goto_mode/$goto_sel"
+				[ $? = 255 ] && exit 255 # force-quit
+			fi
 		fi
 	done
 ;;
